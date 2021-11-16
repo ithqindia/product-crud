bfc6c65f096745010447755c0955c8a10ece572d
Amol Wankhede <amolsw.nz@gmail.com>
2021-11-16 18:52:18 +0530

    update

--- edit.php
+++ edit.php
@@ -10,7 +10,7 @@ $result = $stmt->fetch();
 
 <!-- update -->
 <?php
-if($_POST){
+if ($_POST) {
 
     $sql = "UPDATE product SET name=?, price=?, discount=?, category=?, image=?, description=? WHERE id =?";
     $stmt = $pdo->prepare($sql);
@@ -27,74 +27,76 @@ if($_POST){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php
+    <?php
     $title = 'update page';
     include 'inc-head.php';
     ?>
 </head>
-<body>
-    
 
-<?php include 'inc-nav.php' ?>
+<body>
+
+
+    <?php include 'inc-nav.php' ?>
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <h1>Add product!</h1>
                 <!-- form start -->
-                <form action="edit.php?id=<?php echo $_GET['id']?>" method="post">
-                <!-- name -->
-                <div class="mb-3">
-                 <label for="formGroupExampleInput" class="form-label">Name :</label>
-                 <input type="text" name="name" value="<?php echo $result['name']?>" class="form-control" id="formGroupExampleInput" placeholder="Name">
-                </div>
-                <!-- price -->
-                <div class="mb-3">
-                 <label for="formGroupExampleInput2" class="form-label">Price :</label>
-                 <input type="number" name="price" value="<?php echo $result['price']?>" class="form-control" id="formGroupExampleInput2" placeholder="Price">
-                </div>
-                <!-- discount -->
-                <fieldset class="row mb-3">
-                  <legend class="col-form-label col-sm-2 pt-0">Discount :</legend>
-                  <div class="col-sm-10">
-                    <div class="form-check">
-                     <input class="form-check-input" type="radio" name="discount" id="1" value="10"> 
-                     <label class="form-check-label" for="gridRadios1">
-                      10%
-                     </label>
-                   </div>
-                   <div class="form-check">
-                     <input class="form-check-input" type="radio" name="discount" id="2" value="20">
-                     <label class="form-check-label" for="gridRadios2">
-                      20%
-                     </label>
-                   </div>
-                   <div class="form-check disabled">
-                     <input class="form-check-input" type="radio" name="discount" id="3" value="30">
-                     <label class="form-check-label" for="gridRadios3">
-                      30%
-                   </label>
-                  </div>
-                 </div>
-                </fieldset>
-                <!-- category -->
-                <div class="mb-3">
-                 <label for="formGroupExampleInput2" class="form-label">Category :</label>
-                 <input type="number"  name="category" value="<?php echo $result['category']?>" class="form-control" id="formGroupExampleInput2" placeholder="Category">
-                </div>
-                <!-- image -->
-                <div class="mb-3">
-                 <label for="formGroupExampleInput2" class="form-label">Image:</label>
-                 <input type="number" name="image"  value="<?php echo $result['image']?> " class="form-control" id="formGroupExampleInput2" placeholder="Image">
-                </div>
-                <!-- description -->
-                <div class="mb-3">
-                 <label for="formGroupExampleInput2" class="form-label">Description:</label><br>
-                 <textarea name="description" value="<?php echo $result['description']?>" id="" cols="30" rows="10" placeholder="write here.."></textarea> 
-                 
-                </div>
-                <!-- submit -->
-                <button type="submit" class="btn btn-warning">Updte</button>
+                <form action="edit.php?id=<?php echo $_GET['id'] ?>" method="post">
+                    <!-- name -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput" class="form-label">Name :</label>
+                        <input type="text" name="name" value="<?php echo $result['name'] ?>" class="form-control" id="formGroupExampleInput" placeholder="Name">
+                    </div>
+                    <!-- price -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Price :</label>
+                        <input type="number" name="price" value="<?php echo $result['price'] ?>" class="form-control" id="formGroupExampleInput2" placeholder="Price">
+                    </div>
+                    <!-- discount -->
+                    <fieldset class="row mb-3">
+                        <legend class="col-form-label col-sm-2 pt-0">Discount :</legend>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="discount" id="1" value="10" <?php if($result['discount'] == 10) echo "checked" ?>>
+                                <label class="form-check-label" for="gridRadios1">
+                                    10%
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="discount" id="2" value="20" <?php if($result['discount'] == 20) echo "checked" ?>>
+                                <label class="form-check-label" for="gridRadios2">
+                                    20%
+                                </label>
+                            </div>
+                            <div class="form-check disabled">
+                                <input class="form-check-input" type="radio" name="discount" id="3" value="30" <?php if($result['discount'] == 30) echo "checked" ?>>
+                                <label class="form-check-label" for="gridRadios3">
+                                    30%
+                                </label>
+                            </div>
+                        </div>
+                    </fieldset>
+                    <!-- category -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Category :</label>
+                        <input type="number" name="category" value="<?php echo $result['category'] ?>" class="form-control" id="formGroupExampleInput2" placeholder="Category">
+                    </div>
+                    <!-- image -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Image:</label>
+                        <input type="text" name="image" value="<?php echo $result['image'] ?> " class="form-control" id="formGroupExampleInput2" placeholder="Image">
+                    </div>
+                    <!-- description -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Description:</label><br>
+                        <textarea name="description" id="" cols="30" rows="10" placeholder="write here.."><?php echo $result['description'] ?></textarea>
+
+                    </div>
+                    <!-- submit -->
+                    <button type="submit" class="btn btn-warning">Updte</button>
                 </form>
             </div>
         </div>
@@ -112,8 +114,6 @@ if($_POST){
     -->
 </body>
 
-    
+
 
 </html>
-
-
